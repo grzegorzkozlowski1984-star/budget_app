46d4399406288dd8cd5c7c2b1dcac28a5df8d24c
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-19 23:34:46 +0100

    Create NavActivity.kt

---
+++ app/src/main/java/com/example/compose/jetchat/NavActivity.kt
@@ -0,0 +1,35 @@
+package com.example.compose.jetchat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.jetchat.ui.categories.CategoriesScreen
+
+class NavActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AppContent()
+        }
+    }
+}
+
+@Composable
+fun AppContent() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "categories"
+    ) {
+        composable("categories") {
+            CategoriesScreen()
+        }
+    }
+}
