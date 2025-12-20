a449914daa7e48e6a1354b1532d96fcae1085182
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-21 00:50:27 +0100

    Update TransactionDao.kt

--- app/src/main/java/com/example/compose/jetchat/data/local/dao/TransactionDao.kt
+++ app/src/main/java/com/example/compose/jetchat/data/local/dao/TransactionDao.kt
@@ -1,11 +1,16 @@
 package com.example.compose.jetchat.data.local.dao
 
-import androidx.room.*
-import com.example.compose.jetchat.data.local.entity.TransactionEntity
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.compose.jetchat.domain.model.Transaction
 
 @Dao
 interface TransactionDao {
 
     @Insert
-    suspend fun insert(transaction: TransactionEntity): Long
+    suspend fun insert(transaction: Transaction)
+
+    @Query("SELECT * FROM transactions")
+    suspend fun getAll(): List<Transaction>
 }
