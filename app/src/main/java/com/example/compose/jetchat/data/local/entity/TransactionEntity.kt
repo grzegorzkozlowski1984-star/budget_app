144e27b47a601acc0ffc964db78c134acca1bea6
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-21 00:27:12 +0100

    Update TransactionEntity.kt

--- app/src/main/java/com/example/compose/jetchat/data/local/entity/TransactionEntity.kt
+++ app/src/main/java/com/example/compose/jetchat/data/local/entity/TransactionEntity.kt
@@ -1,28 +1,16 @@
 package com.example.compose.jetchat.data.local.entity
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import java.time.LocalDateTime
+import androidx.room.*
+import java.math.BigDecimal
+import java.time.LocalDate
 
-@Entity(tableName = "transactions")
+@Entity(
+    tableName = "transactions"
+)
 data class TransactionEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0L,
-
-    /**
-     * Data i czas transakcji
-     */
-    val dateTime: LocalDateTime,
-
-    /**
-     * Opis / sklep / notatka
-     */
-    val description: String? = null,
-
-    /**
-     * Całkowita kwota transakcji
-     * (suma ExpenseItem.amount)
-     */
-    val totalAmount: Double
+    val id: Long = 0,
+    val date: LocalDate,
+    val description: String?,
+    val totalAmount: BigDecimal
 )
-
