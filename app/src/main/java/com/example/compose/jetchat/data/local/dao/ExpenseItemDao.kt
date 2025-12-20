c319f3ac0247c76661c9617c877585dcf8f69e4d
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-21 00:50:47 +0100

    Update ExpenseItemDao.kt

--- app/src/main/java/com/example/compose/jetchat/data/local/dao/ExpenseItemDao.kt
+++ app/src/main/java/com/example/compose/jetchat/data/local/dao/ExpenseItemDao.kt
@@ -1,11 +1,16 @@
 package com.example.compose.jetchat.data.local.dao
 
-import androidx.room.*
-import com.example.compose.jetchat.data.local.entity.ExpenseItemEntity
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.compose.jetchat.domain.model.ExpenseItem
 
 @Dao
 interface ExpenseItemDao {
 
     @Insert
-    suspend fun insertAll(items: List<ExpenseItemEntity>)
+    suspend fun insert(item: ExpenseItem)
+
+    @Query("SELECT * FROM expense_items")
+    suspend fun getAll(): List<ExpenseItem>
 }
