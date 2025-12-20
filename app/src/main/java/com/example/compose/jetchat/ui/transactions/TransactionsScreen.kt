8be7531b5a4cf2eb218df0d84edf2488100c397b
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-20 04:01:28 +0100

    Update TransactionsScreen.kt

--- app/src/main/java/com/example/compose/jetchat/ui/transactions/TransactionsScreen.kt
+++ app/src/main/java/com/example/compose/jetchat/ui/transactions/TransactionsScreen.kt
@@ -1,1 +1,9 @@
-package com.example.compose.jetchat.ui.transactions
+package com.example.compose.jetchat.ui.settings
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen() {
+    Text("Settings screen")
+}
