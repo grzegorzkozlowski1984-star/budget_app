56653b491da6e1e7fb0eb72f6502f620c3464826
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-21 01:00:36 +0100

    Update AppDatabase.kt

--- app/src/main/java/com/example/compose/jetchat/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/compose/jetchat/data/local/AppDatabase.kt
@@ -3,6 +3,12 @@ package com.example.compose.jetchat.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.compose.jetchat.data.local.dao.CategoryDao
+import com.example.compose.jetchat.data.local.dao.TransactionDao
+import com.example.compose.jetchat.data.local.dao.ExpenseItemDao
+import com.example.compose.jetchat.domain.model.Category
+import com.example.compose.jetchat.domain.model.Transaction
+import com.example.compose.jetchat.domain.model.ExpenseItem
 import com.example.compose.jetchat.data.local.entity.*
 
 @Database(
