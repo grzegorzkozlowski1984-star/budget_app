4b5a27da4c8921f3c7253028271573c2b0770135
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-21 21:00:44 +0100

    Update ExpenseSumByLabel.kt

--- app/src/main/java/com/example/compose/jetchat/data/local/query/ExpenseSumByLabel.kt
+++ app/src/main/java/com/example/compose/jetchat/data/local/query/ExpenseSumByLabel.kt
@@ -1,7 +1,8 @@
 package com.example.compose.jetchat.data.local.query
 
 data class ExpenseSumByLabel(
-    val labelId: Long,
-    val labelName: String,
-    val totalAmount: Double
+    val labelId: Long?,
+    val labelName: String?,
+    val sum: Double
+
 )
