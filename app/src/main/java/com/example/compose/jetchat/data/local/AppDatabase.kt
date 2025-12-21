f1a3f5119d6d7bd16398582895eaeb7e4c97f56a
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-21 19:32:01 +0100

    Update AppDatabase.kt

--- app/src/main/java/com/example/compose/jetchat/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/compose/jetchat/data/local/AppDatabase.kt
@@ -6,23 +6,33 @@ import androidx.room.TypeConverters
 import com.example.compose.jetchat.data.local.dao.CategoryDao
 import com.example.compose.jetchat.data.local.dao.TransactionDao
 import com.example.compose.jetchat.data.local.dao.ExpenseItemDao
+import com.example.compose.jetchat.data.local.dao.LabelDao
+import com.example.compose.jetchat.data.local.dao.ExpenseItemLabelDao
 import com.example.compose.jetchat.domain.model.Category
 import com.example.compose.jetchat.domain.model.Transaction
 import com.example.compose.jetchat.domain.model.ExpenseItem
 import com.example.compose.jetchat.data.local.entity.*
+import com.example.compose.jetchat.data.local.entity.LabelEntity
+import com.example.compose.jetchat.data.local.entity.ExpenseItemLabelCrossRef
 
 @Database(
     entities = [
         CategoryEntity::class,
         TransactionEntity::class,
-        ExpenseItemEntity::class
+        ExpenseItemEntity::class,
+        LabelEntity::class,
+        ExpenseItemLabelCrossRef::class
     ],
-    version = 1
+    version = 2
 )
+
 @TypeConverters(TypeConverters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun categoryDao(): CategoryDao
     abstract fun transactionDao(): TransactionDao
     abstract fun expenseItemDao(): ExpenseItemDao
+    abstract fun labelDao(): LabelDao
+    abstract fun expenseItemLabelDao(): ExpenseItemLabelDao
+
 }
