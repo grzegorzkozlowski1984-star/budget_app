1f13807faae36318ed5b3b65264ad557be2b360f
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-21 19:23:31 +0100

    Create LabelEntity.kt

---
+++ app/src/main/java/com/example/compose/jetchat/data/local/entity/LabelEntity.kt
@@ -0,0 +1,18 @@
+package com.example.compose.jetchat.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "labels",
+    indices = [
+        Index(value = ["name"], unique = true)
+    ]
+)
+data class LabelEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val isSystem: Boolean = false
+)
