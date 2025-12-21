0a12c069c44700cc308e5b06d0a0c8a361373a11
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-21 19:48:04 +0100

    Create ExpenseSumByLabelAndCategory.kt

---
+++ app/src/main/java/com/example/compose/jetchat/data/local/query/ExpenseSumByLabelAndCategory.kt
@@ -0,0 +1,9 @@
+package com.example.compose.jetchat.data.local.query
+
+data class ExpenseSumByLabelAndCategory(
+    val labelId: Long,
+    val labelName: String,
+    val categoryId: Long,
+    val categoryName: String,
+    val totalAmount: Double
+)
