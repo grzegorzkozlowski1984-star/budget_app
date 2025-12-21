af467d261c4964e1b7288f60496b6c9b24c9ca95
grzegorzkozlowski1984-star <grzegorz.kozlowski1984@gmail.com>
2025-12-21 19:47:21 +0100

    Update ExpenseItemDao.kt

--- app/src/main/java/com/example/compose/jetchat/data/local/dao/ExpenseItemDao.kt
+++ app/src/main/java/com/example/compose/jetchat/data/local/dao/ExpenseItemDao.kt
@@ -17,13 +17,35 @@ interface ExpenseItemDao {
         WHERE categoryId = :categoryId
     """)
     suspend fun sumForCategory(categoryId: Long): Long?
-        @Query(
+    @Query(
         """
         SELECT COUNT(*) > 0
         FROM expense_items
         WHERE categoryId = :categoryId
-        """
-    )
-    
+    """)
     suspend fun hasItemsForCategory(categoryId: Long): Boolean
+
+    @Query("""
+    SELECT 
+        l.id AS labelId,
+        l.name AS labelName,
+        SUM(e.amount) AS totalAmount
+    FROM expense_items e
+    JOIN expense_item_label_cross_ref x
+        ON e.id = x.expenseItemId
+    JOIN labels l
+        ON l.id = x.labelId
+    GROUP BY l.id, l.name
+    """)
+    suspend fun sumExpensesByLabel(): List<ExpenseSumByLabel>
+
+    @Query("""
+    SELECT 
+        SUM(e.amount)
+    FROM expense_items e
+    LEFT JOIN expense_item_label_cross_ref x
+        ON e.id = x.expenseItemId
+    WHERE x.expenseItemId IS NULL
+    """)
+    suspend fun sumExpensesWithoutLabel(): Double?
 }
